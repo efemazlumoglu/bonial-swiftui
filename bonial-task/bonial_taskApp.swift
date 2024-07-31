2d8b899a87ee4c0daafe7fb8dbfad3597d41b8f9
efemazlumoglu <efe.mazlumoglu@gmail.com>
2024-07-31 12:14:05 +0200

    initial commit

---
+++ bonial-task/bonial_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  bonial_taskApp.swift
+//  bonial-task
+//
+//  Created by Efe Maz on 31.07.24.
+//
+
+import SwiftUI
+
+@main
+struct bonial_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
